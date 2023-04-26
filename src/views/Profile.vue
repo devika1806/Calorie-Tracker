4fd7fac5db7c5bd4b8602ac54edefcef35240387
devarshukani <me.devarshukani@gmail.com>
2023-04-26 17:16:43 +0530

    Profile page connected to firebase

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="profile-container">
       <h1>Create Profile</h1>
-      <form>
+      <!-- <form> -->
         <div class="form-group">
           <label for="name">Name:</label>
-          <input type="text" id="name" v-model="name">
+          <input type="text" id="name" v-model="profile.name">
         </div>
         <div class="form-group">
           <label for="age">Age:</label>
-          <input type="number" id="age" v-model="age">
+          <input type="number" id="age" v-model="profile.age">
         </div>
         <div class="form-group">
           <label for="gender">Gender:</label>
-          <select id="gender" v-model="gender">
+          <select id="gender" v-model="profile.gender">
             <option value="">--Please select--</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
@@ -21,15 +21,15 @@
         </div>
         <div class="form-group">
           <label for="height">Height (cm):</label>
-          <input type="number" id="height" v-model="height">
+          <input type="number" id="height" v-model="profile.height">
         </div>
         <div class="form-group">
           <label for="weight">Weight (kg):</label>
-          <input type="number" id="weight" v-model="weight">
+          <input type="number" id="weight" v-model="profile.weight">
         </div>
         <div class="form-group">
           <label for="activity-level">Activity Level:</label>
-          <select id="activity-level" v-model="activityLevel">
+          <select id="activity-level" v-model="profile.activityLevel">
             <option value="">--Please select--</option>
             <option value="sedentary">Sedentary</option>
             <option value="lightly-active">Lightly Active</option>
@@ -38,44 +38,79 @@
             <option value="extra-active">Extra Active</option>
           </select>
         </div>
-        <button class="btn" @click.prevent="createProfile">Create Profile</button>
-      </form>
+        <button class="btn" @click="createProfile">Save Changes</button>
+      <!-- </form> -->
     </div>
   </template>
   
   <script>
-  import firebase from 'firebase'
+  // import firebase from 'firebase'
+  import firebase from "firebase/app";
+  import "firebase/auth";
+  import "firebase/database";
 
   export default {
     data() {
       return {
-        name: "",
-        age: null,
-        gender: "",
-        height: null,
-        weight: null,
-        activityLevel: "",
+        profile: {
+          name: "",
+          age: null,
+          gender: "",
+          height: null,
+          weight: null,
+          activityLevel: "",
+          uid: null,
+        }
       };
     },
     methods: {
       createProfile() {
-        const profile = {
-          name: this.name,
-          age: this.age,
-          gender: this.gender,
-          height: this.height,
-          weight: this.weight,
-          activityLevel: this.activityLevel,
-        };
-  
-        console.log(profile); // Replace with your API call to create the profile
+
+        fetch('https://calorie-tracker-1bf51-default-rtdb.asia-southeast1.firebasedatabase.app/users/'+this.uid+'/.json', {
+          method: 'POST',
+          headers : {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+           uid: this.profile
+        })  
+        });
+
+        this.$router.push("/");
+
       },
     },
     created() {
     firebase.auth().onAuthStateChanged((user) => {
-      console.log("user : ",user);
       if (!user) {
         this.$router.push("/");
+      }
+      else{
+        this.uid = user.uid;
+        console.log(this.uid);
+
+        fetch('https://calorie-tracker-1bf51-default-rtdb.asia-southeast1.firebasedatabase.app/users/'+this.uid+'/.json').then((response) => {
+            if (response.ok){
+              return response.json();
+            }
+          })
+          .then((data) => {
+            console.log(data);
+            
+            const results = [];
+            var temp = -1
+            for (const id in data) {
+              temp+=1;
+              results.push({
+                id: id,
+                uid: data[id]['uid']
+              });
+            }
+
+            this.profile = results[temp].uid;
+            console.log(results);
+          });
+
       }
     });
   },
@@ -121,7 +156,7 @@
   }
   
   .btn {
-    background-color: #0069d9;
+    background-color: #4caf50;
     color: white;
     padding: 10px 20px;
     border: none;
